23822a4cb62e2006bb932df4e931b4ec13c9a6f8
Xuningsan <xuningsan@sina.com>
2022-12-24 23:50:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git dockerman  https://github.com/lisaac/luci-app-dockerman.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
